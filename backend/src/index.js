eea9e03531c02b3ac960eab793c466446a28de5f
SpyrosStav <spyrosstav99@gmail.com>
2026-06-10 19:37:27 +0300

    Add db migration tables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,46 @@
 import express from "express";
+import session from "express-session";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./swagger.js";
+import sequelize from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(express.json());
+
+app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 3600 * 1000 },
+    })
+);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
     res.send("API running");
 });
 
+(async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log("DB Connected & Models Synced!");
+    } catch (err) {
+        console.error("DB Error:", err);
+    }
+})();
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
---
+++ backend/src/swagger.js
@@ -0,0 +1,20 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Shreddeded Swagger",
+            version: "1.0.0",
+            description: "API documentation",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./src/routes/*.js"],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
